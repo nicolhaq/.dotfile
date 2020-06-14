e01743a0c64a544101e8424c396440ac6a9bad39
nhaquet <nicolhaq@gmail.com>
2020-06-15 01:46:40 +0200

    [ADD] update polybar and wallpaper

--- .config/fish/functions/backyay.fish
+++ .config/fish/functions/backyay.fish
@@ -1,4 +1,4 @@
 # Defined in - @ line 1
-function backyay --description 'alias backyay=yay -Qqe >> ~/.config/installed_packet'
+function backyay --description 'alias backyay=yay -Qqe > ~/.config/installed_packet'
 	yay -Qqe > ~/.config/installed_packet $argv;
 end
--- .config/polybar/init.sh
+++ .config/polybar/init.sh
@@ -5,7 +5,12 @@ killall -q polybar
 # If all your bars have ipc enabled, you can also use 
 # polybar-msg cmd quit
 
-# Launch bar1 and bar2
+# Launch bar1 and bar2for i in /sys/class/hwmon/hwmon*/temp*_input; do 
+for i in /sys/class/hwmon/hwmon*/temp*_input; do 
+    if [ "$(<$(dirname $i)/name): $(cat ${i%_*}_label 2>/dev/null || echo $(basename ${i%_*}))" = "k10temp: Tdie" ]; then
+	export HWMON_PATH="$i"
+    fi
+done
 echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
 polybar top >>/tmp/polybar1.log 2>&1 &
 polybar bottom >>/tmp/polybar2.log 2>&1 &
